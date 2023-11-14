65c84b072b33819dd4bda0e5e72109291dab3c14
CodingCraft <codingcraftyt@gmail.com>
2023-11-14 20:02:41 +0530

    Update custom file path in .py file

--- youtube_downloader.py
+++ youtube_downloader.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 from pytube import YouTube
 import customtkinter
-
+from tkinter import filedialog
 
 def startDownload(option):
     try:
@@ -9,16 +9,22 @@ def startDownload(option):
         ytObject = YouTube(ytLink, on_progress_callback=on_progress)
         if option == "hq":
             video = ytObject.streams.get_highest_resolution()
+            file_path_hq = filedialog.asksaveasfilename(defaultextension=".mp4", filetypes=[("MP4 files", "*.mp4")])
+            video.download(file_path_hq)
         elif option == "lq":
             video = ytObject.streams.get_lowest_resolution()
+            file_path_lq = filedialog.asksaveasfilename(defaultextension=".mp4", filetypes=[("MP4 files", "*.mp4")])
+            video.download(file_path_lq)
         elif option == "audio":
             video = ytObject.streams.get_audio_only()
+            file_path_audio = filedialog.asksaveasfilename(defaultextension=".mp3", filetypes=[("Audio files", "*.mp3")])
+            video.download(file_path_audio)
         else:
             return
 
         title.configure(text=ytObject.title, text_color="white")
         finishLabel.configure(text="")
-        video.download()
+
         finishLabel.configure(text="Downloaded !!", text_color="green")
     except:
         finishLabel.configure(text="Download Error", text_color="red")
